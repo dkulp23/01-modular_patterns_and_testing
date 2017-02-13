37c76116327154be0e93541b2764370486146f22
Dana Kulp <dkulp23@gmail.com>
2017-02-13 15:50:23 -0800

    Testing out process.argv properties to determine how to handle multiple inputs

--- index.js
+++ index.js
@@ -3,5 +3,8 @@
 const welcome = require('./lib/howdy.js');
 
 if (process.argv.length < 2) throw new Error('please enter a name!');
+console.log(process.argv[2]);
+console.log(process.argv[2].length);
+// if (process.argv.length > 3) welcome.multipleGreetings(process.argv);
 
 welcome.greeting(process.argv[2]);
