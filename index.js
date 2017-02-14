55e2db754ff0b775fb6f405880685bfa74ec17dd
Dana Kulp <dkulp23@gmail.com>
2017-02-13 16:20:27 -0800

    One test written for each method. Passing both.

--- index.js
+++ index.js
@@ -3,10 +3,7 @@
 const welcome = require('./lib/howdy.js');
 
 if (process.argv.length < 2) throw new Error('please enter a name!');
-// console.log(process.argv);
-// console.log(process.argv[2]);
-// console.log(process.argv[3]);
-console.log(process.argv.length);
+
 if (process.argv.length === 3) welcome.greeting(process.argv[2]);
 
 if (process.argv.length > 3) welcome.multipleGreetings(process.argv);
--- test/howdy-test.js
+++ test/howdy-test.js
@@ -7,7 +7,13 @@ describe('Howdy Module', function() {
   describe('#greeting', function() {
     it(`should return hello ${process.argv[2]}`, function() {
       var printout = howdy.greeting(process.argv[2]);
-      assert.ok(printout === `hello ${process.argv[2]}`, 'not equal to hello Dana');
+      assert.ok(printout === `hello ${process.argv[2]}`, 'incorrect result');
+    });
+  });
+  describe('#multipleGreetings', function() {
+    it(`should return all names entered in command line`, function() {
+      var printout = howdy.multipleGreetings(process.argv);
+      assert.ok(printout === `Hey there ${process.argv.slice(2).join(' and ')}`, 'not quite right');
     });
   });
 });
